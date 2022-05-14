31366031897e11ba59bb70df8187ce76be939ba9
cmr1357 <79671395+cmr1357@users.noreply.github.com>
2022-05-14 12:31:18 +0530

    Add files via upload

---
+++ Data Types/Strings/Check_String.py
@@ -0,0 +1,17 @@
+#check string
+
+string="Passion never fails"
+
+print("never" in string)
+
+if"Passion" in string:
+    print("Yes")
+ 
+ 
+ #Check if NOT
+    
+print("Expensive" not in string)
+
+if "Expensive" not in string:
+    print("No, expensive is not present")
+    
