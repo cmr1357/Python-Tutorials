b19e018a83702630a27c4e4754ef1d34d1457b81
cmr1357 <79671395+cmr1357@users.noreply.github.com>
2022-05-14 12:32:50 +0530

    Add files via upload

---
+++ Data Types/Strings/Slicing_String.py
@@ -0,0 +1,18 @@
+#Slicing a string
+
+string="Passion never fails"
+
+print(string[8:19])
+
+#Slice from the start
+
+print(string[:8])
+
+#Slice to the End
+
+print(string[8:])
+
+#Negative Indexing
+
+print(string[-19:8])
+
