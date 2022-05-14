d525b78375f963c72d47570d5af31c2360b0b4c8
cmr1357 <79671395+cmr1357@users.noreply.github.com>
2022-05-14 12:32:05 +0530

    Add files via upload

---
+++ Data Types/Strings/Modify_String.py
@@ -0,0 +1,43 @@
+#Upper Case
+
+string="Hello"
+print(string.upper())
+
+#Lower case
+
+print(string.lower())
+
+#Remove Whitespace
+
+string=" Hello   World"
+print(string.strip())
+
+#Replace String
+
+string="Hello World"
+
+print(string.replace("H","W"))
+
+#Split String
+
+print(string.split("o"))
+
+#Concatenation
+
+a="Passion "
+b="Never fails"
+c=a+b
+
+print(c)
+
+#Formating a string
+
+age=32
+
+string="I am {}"
+print(string.format(age))
+
+#Escape character
+
+string="Your best teacher is your \"Last mistake.\""
+print(string)
