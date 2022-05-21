7bb67d43bcb67c363fd5a0d654eb576ad092b195
cmr1357 <79671395+cmr1357@users.noreply.github.com>
2022-05-21 05:39:44 +0530

    Add files via upload

---
+++ Data Types/List/List_Items.py
@@ -0,0 +1,13 @@
+#List Data
+
+list1=["Apple","Banana","Orange"]
+list2=[1,5,7,9,3]
+list3=[True,False,False]
+list4=["abc",34,True,40]
+
+print(list1)
+print(list2)
+print(list3)
+print(list4)
+
+print(type(list1))
