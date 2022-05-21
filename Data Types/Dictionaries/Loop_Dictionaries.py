79720f23cf308e5e634cfa26aa620e3dee6dee8f
cmr1357 <79671395+cmr1357@users.noreply.github.com>
2022-05-21 16:50:59 +0530

    Add files via upload

---
+++ Data Types/Dictionaries/Loop_Dictionaries.py
@@ -0,0 +1,17 @@
+dict1={"Color":"Yellow","Item No":123}
+for x in dict1:
+    print(x)
+    
+for x in dict1:
+    print(dict1[x])
+    
+    
+for x in dict1.values():
+    print(x)
+    
+    
+for x in dict1.keys():
+    print(x)
+
+for x ,y in dict1.items():
+    print(x,y)
