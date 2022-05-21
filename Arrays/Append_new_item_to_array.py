f74495966b7267912716caa8fab0ed89184b561d
cmr1357 <79671395+cmr1357@users.noreply.github.com>
2022-05-21 19:23:11 +0530

    Add files via upload

---
+++ Arrays/Append_new_item_to_array.py
@@ -0,0 +1,10 @@
+#Append new item to end of array
+
+from array import *
+
+array1=array("i",[1,2,3,4,5,6,7])
+print("Original array :"+str(array1))
+
+print("Apped 8 at the end of the array")
+array1.append(8)
+print("New array : "+str(array1))
