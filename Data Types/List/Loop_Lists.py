5d53c64441522acc2159b5e8f5bab8017d1cce58
cmr1357 <79671395+cmr1357@users.noreply.github.com>
2022-05-21 05:40:11 +0530

    Add files via upload

---
+++ Data Types/List/Loop_Lists.py
@@ -0,0 +1,13 @@
+#Loop through a list
+
+list1=["Apple","Orange","Banana","Mango"]
+
+for x in list1:
+    print(x)
+    
+
+#Loop through Index Numbers
+    
+for i in range(len(list1)):
+    print(list1[i])
+    
