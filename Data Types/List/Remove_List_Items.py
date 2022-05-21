c98ebc747c0112800426fbb837bec605e02dac71
cmr1357 <79671395+cmr1357@users.noreply.github.com>
2022-05-21 05:41:06 +0530

    Add files via upload

---
+++ Data Types/List/Remove_List_Items.py
@@ -0,0 +1,20 @@
+#Remove List Items
+
+#remove method
+list1=["Apple","Orange","Banana"]
+list1.remove("Orange")
+print(list1)
+
+
+#pop method
+list1.pop(1)
+print(list1)
+
+#clear method
+list1.clear()
+print(list1)
+
+#del keyword
+list1=["Apple","Orange","Banana"]
+del list1[0]
+print(list1)
