75dc1f401b0b8d3fd982db0567b31c3d9abbc862
cmr1357 <79671395+cmr1357@users.noreply.github.com>
2022-05-21 18:12:17 +0530

    Add files via upload

---
+++ Conditionals/Given_number_positive_negative_zero.py
@@ -0,0 +1,11 @@
+#Given number is positive or negative or zero
+
+num=int(input("Enter the number"))
+
+if num>0:
+    print("The given number is positive")
+elif num==0:
+    print("The given number is zero")
+else:
+    print("Given number is a negative number")
+    
