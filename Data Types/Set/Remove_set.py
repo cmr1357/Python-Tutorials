74dfc8f35bbe5133a1fb0acf50a8cbb64345e779
cmr1357 <79671395+cmr1357@users.noreply.github.com>
2022-05-21 14:10:14 +0530

    Add files via upload

---
+++ Data Types/Set/Remove_set.py
@@ -0,0 +1,29 @@
+#Using Remove Method
+set1={"apple","Orange","cherry"}
+set1.remove("Orange")
+
+print(set1)
+
+#discard method
+set1={"apple","Orange","cherry"}
+set1.discard("Orange")
+print(set1)
+
+#pop method
+set1={"apple","Orange","cherry"}
+x=set1.pop()
+
+print(x)
+print(set1)
+
+
+set1.clear()
+
+print(set1)
+
+
+set1={"apple","Orange","cherry"}
+
+del set1
+print(set1)
+
