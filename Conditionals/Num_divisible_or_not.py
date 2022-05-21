6569ed3d671b31d32ea3ec5c9fe8c049ad042ded
cmr1357 <79671395+cmr1357@users.noreply.github.com>
2022-05-21 18:10:37 +0530

    Add files via upload

---
+++ Conditionals/Num_divisible_or_not.py
@@ -0,0 +1,8 @@
+#number is divisible or not
+num=int(input("Enter the number"))
+div=int(input("Enter the second number"))
+
+if num%div ==0:
+    print(num,"is divisible by",div)
+else:
+    print(num,"is not divisible by",div)
