eefee6e3595619f0c24da2d494e4cce1162b774a
cmr1357 <79671395+cmr1357@users.noreply.github.com>
2022-05-21 13:25:37 +0530

    Add files via upload

---
+++ Data Types/Tuple/Access_Tuples.py
@@ -0,0 +1,18 @@
+#Tuples
+
+tuple1=("Apple","Banana","Orange","apple","Cherry")
+print(tuple1)
+
+#Access Tuples
+print(tuple1[1])
+
+#Negative Indexing
+print(tuple1[-1])
+
+
+#Range of Indexes
+print(tuple1[2:4])
+
+print(tuple1[:2])
+
+print(tuple1[1:])
