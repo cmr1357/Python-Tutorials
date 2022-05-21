a7f701f6e413101e727bd749500872a165b2e12d
cmr1357 <79671395+cmr1357@users.noreply.github.com>
2022-05-21 14:08:59 +0530

    Add files via upload

---
+++ Data Types/Set/Add_set_items.py
@@ -0,0 +1,14 @@
+# Add set items
+
+set1={"Apple","Orange","banana"}
+set1.add("Pineapple")
+
+print(set1)
+
+
+set1={"Apple","Orange","banana"}
+set2={"Red","Orange","blue"}
+
+set1.update(set2)
+
+print(set1)
