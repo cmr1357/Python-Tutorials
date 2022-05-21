e63cf5c596d0f90bb9166377895c955674043e4c
cmr1357 <79671395+cmr1357@users.noreply.github.com>
2022-05-21 18:49:50 +0530

    Add files via upload

---
+++ Loops/While_Loop/beak_and_continue_ while_ loop.py
@@ -0,0 +1,18 @@
+n=1
+while n<=10:
+    print(n)
+    n=n+1
+    if n==5:
+        break
+print("done")
+
+
+print("\n")
+print("continue")
+n=1
+while n<=10:
+    print(n)
+    n=n+1
+    if n==5:
+        continue
+print("done")
