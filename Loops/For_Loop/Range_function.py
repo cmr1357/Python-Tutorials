318fb0c10a9f95d8c3082460fbd8212962a3423c
cmr1357 <79671395+cmr1357@users.noreply.github.com>
2022-05-21 18:40:04 +0530

    Add files via upload

---
+++ Loops/For_Loop/Range_function.py
@@ -0,0 +1,28 @@
+##range function
+
+x=range(10)
+
+for i in x:
+    print(i)
+
+
+print("\n")
+
+##range(start,stop,step)
+
+
+x=range(3,30)
+
+for i in x:
+    print(i)
+
+
+print("\n")
+
+x=range(10,100,5)
+
+for i in x:
+    print(i)
+
+
+print("\n")
