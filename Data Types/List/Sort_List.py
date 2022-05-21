37c26c956f74e716fc768f13ae38599aaf67f591
cmr1357 <79671395+cmr1357@users.noreply.github.com>
2022-05-21 05:40:37 +0530

    Add files via upload

---
+++ Data Types/List/Sort_List.py
@@ -0,0 +1,16 @@
+#Sort List
+
+list1=["Apple","Orange","Banana","cherry"]
+list1.sort()
+print(list1)
+
+list2=[100,500,300,800,1000]
+list2.sort()
+print(list2)
+
+
+#Sort Descending
+list1=["Apple","Orange","Banana","cherry"]
+list1.sort(reverse=True)
+print(list1)
+
