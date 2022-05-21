b03e8c7f8a0744f1deee5f0b82583d09343b4b3b
cmr1357 <79671395+cmr1357@users.noreply.github.com>
2022-05-21 18:10:58 +0530

    Add files via upload

---
+++ Conditionals/Number_entered_is_multiple_of_five.py
@@ -0,0 +1,7 @@
+#NUmber entered is multiple of five
+num=int(input("Enter a number"))
+
+if num%5==0:
+    print("The number entered is multiple of five")
+else:
+    print("The number entered is not an multiple of five")
