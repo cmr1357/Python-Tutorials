ab3ba33d4b908a26aab552e35e416304944f87aa
cmr1357 <79671395+cmr1357@users.noreply.github.com>
2022-05-21 13:31:12 +0530

    Add files via upload

---
+++ Data Types/Tuple/More_Practice_Problems/Reverse_a_tuple.py
@@ -0,0 +1,11 @@
+#Reverse a tuple
+tuple1=("Canada","NewYork","Africa","India")
+tuplea=reversed(tuple1)
+print(tuple(tuplea))
+
+tuple2=("Welcome")
+tupleb=reversed(tuple2)
+print(tuple(tupleb))
+tuple3=(1,2,3,4,5)
+tuplec=reversed(tuple3)
+print(tuple(tuplec))
