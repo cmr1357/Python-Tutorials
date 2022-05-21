c83e0836f1c6b8d824193df83c33e6db1347170d
cmr1357 <79671395+cmr1357@users.noreply.github.com>
2022-05-21 16:49:16 +0530

    Add files via upload

---
+++ Data Types/Dictionaries/Access_Items.py
@@ -0,0 +1,14 @@
+#Accessing Items by referring to its key
+dict1={"Color":"Yellow","Item No":123}
+
+x=dict1["Color"]
+
+#USing get method
+print(x)
+y=dict1.get("Item No")
+
+#List of all the keys
+
+z=dict1.keys()
+
+print(z)
