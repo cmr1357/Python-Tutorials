2ef434cc6f35118cad45133f6397f512431a0a70
cmr1357 <79671395+cmr1357@users.noreply.github.com>
2022-05-21 18:10:13 +0530

    Add files via upload

---
+++ Conditionals/Leap_Year.py
@@ -0,0 +1,11 @@
+#Leap year or not
+
+year=int(input("Enter a year : "))
+
+if((year %4 ==0 and year %100 !=0) or (year %400 ==0)):
+
+    print("Year is leap year")
+
+else:
+
+    print("Year is not a leap year")
