4d1791bc1d3b8599492a03b827012b2f1cfb5c8b
cmr1357 <79671395+cmr1357@users.noreply.github.com>
2022-05-21 18:11:38 +0530

    Add files via upload

---
+++ Conditionals/Positive_Negative_Zero.py
@@ -0,0 +1,13 @@
+# Given number is positive or negative
+
+n=int(input("Enter a number : "))
+
+if n>0:
+    print("Positive")
+    
+if n<0:
+    print("Negative")
+    
+if n==0:
+    print("Zero")
+    
