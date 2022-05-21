313e1ddfd956b67285e1ba4883d747727ab328af
cmr1357 <79671395+cmr1357@users.noreply.github.com>
2022-05-21 18:35:10 +0530

    Add files via upload

---
+++ Loops/For_Loop/End_keyword.py
@@ -0,0 +1,11 @@
+for i in "india":
+    print(i)
+for i in "india":
+    print(i,end=" ")
+print("\n")
+for i in "india":
+    print(i,end="?")
+for i in "india":
+    print(i,"?")
+for i in "india":
+    print(i,"?$")
